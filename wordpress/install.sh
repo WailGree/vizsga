1e264bfea2e7fbfd2783e92cb1bad1662904aea1
WailGree <szilgyi.roland1@gmail.com>
2019-04-17 08:51:32 +0000

    wordpress telepítése

---
+++ wordpress/install.sh
@@ -0,0 +1,21 @@
+
+echo "Installation of Wordpress has been started."
+apt-get update 
+apt-get install tasksel -y
+tasksel install lamp-server
+echo "SQL rész. Root jelszó igényeltetik.A belépés után egyszerűen írd be, hogy: source <sql.sql helye>"
+mysql -u root -p -Bse "
+CREATE DATABASE wordpressdb;
+CREATE USER wordpressuser@localhost IDENTIFIED BY 'wordpresspassword';
+GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost; FLUSH PRIVILEGES;
+"
+cd /tmp
+wget http://wordpress.org/latest.zip
+unzip -q latest.zip -d /var/www/html/
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress
+mkdir -p /var/www/html/wordpress/wp-content/uploads
+chown -R www-data:www-data /var/www/html/wordpress/wp-content/uploads 
+apt-get install php-mysql -y
+service apache2 restart
+
---
+++ wordpress/sql.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE wordpressdb;
+CREATE USER wordpressuser@localhost IDENTIFIED BY 'wordpresspassword';
+GRANT ALL PRIVILEGES ON wordpressdb.* TO wordpressuser@localhost; FLUSH PRIVILEGES;
